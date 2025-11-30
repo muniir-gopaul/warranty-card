2d2a280f3d8455a974aab92af76f5f20da9c6472
muniir gopaul <muniirwebarts@gmail.com>
2025-11-30 16:21:42 +0400

    dist zip added - corrected IP

---
+++ dist/ssr/client/assets/QCardSection-DIaun5Ia.js
@@ -0,0 +1,1 @@
+import{e,f as r,h as n,i as s}from"./index-C9XCHekm.js";const i=e({name:"QCardSection",props:{tag:{type:String,default:"div"},horizontal:Boolean},setup(t,{slots:o}){const a=r(()=>`q-card__section q-card__section--${t.horizontal===!0?"horiz row no-wrap":"vert"}`);return()=>n(t.tag,{class:a.value},s(o.default))}});export{i as Q};
